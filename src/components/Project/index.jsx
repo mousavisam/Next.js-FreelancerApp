1dee1dc559844efea9a10fa1fcd778964e064c0a
Thamizhazhagan-Murugan <123265927+Thamizhazhagan-Murugan@users.noreply.github.com>
2023-03-28 15:06:22 -0400

    Update index.jsx

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -6,6 +6,7 @@ import {
   Text,
   Input,
   Button,
+
   Select,
 } from "@chakra-ui/react";
 
@@ -15,63 +16,70 @@ export const Project = () => {
       <Box h="60px">
         <Box w="90px" h="50px">
           <Center h="60px">
-            <Text as="b" fontSize="20px">
+            <Text as="b" fontSize={{ base: "md", lg: "lg" }}>
               Skill Tree
             </Text>
           </Center>
         </Box>
       </Box>
-      <Box h="60px" style={{ backgroundColor: "black", color: "white" }}>
-        tell us about your project
+      <Box h="50px"as="b" fontSize={{ base: "lg", lg: "lg" }} style={{ backgroundColor: "#63B3ED", color: "black" }}>
+        
+        Tell us about your project
       </Box>
       <Center>
         <Box h="600px" w="1000px">
           <Text as="b">Name of the project</Text>
           <Input placeholder="eg; Build a virtual game" size="md" />
+          <br></br>
+          <br></br>
           <Text fontSize="20px" color="black">
             Project Description
           </Text>
           <textarea
-            placeholder="Enter descriptyion"
+            placeholder="Enter description"
             rows="10"
             cols="100"
-            style={{ resize: "none" }}
+            style={{ resize: "none", borderColor: "black", }}
+            
           ></textarea>
-          <br />
+          <br></br><br></br>
           <Text as="b">Skills required</Text>
           <Input placeholder="eg; Python, Java" size="md" />
+          <br></br><br></br>
           <Text fontSize="20px" color="black">
             How do you want to pay
           </Text>
           <textarea
-            placeholder="Enter descriptyion"
+            placeholder="Enter description"
             rows="10"
             cols="50"
-            style={{ resize: "none" }}
+            style={{ resize: "lg" }}
           ></textarea>
-          <textarea
-            placeholder="Enter descriptyion"
-            rows="10"
-            cols="50"
-            style={{ resize: "none" }}
-          ></textarea>
+         
           <Text as="b">
+            <br></br>
+            <br></br>
             Estimated budget
+            <br></br><br></br>
+            Budget in per hours
             <Select placeholder="Select option">
               <option value="option1">Option 1</option>
               <option value="option2">Option 2</option>
               <option value="option3">Option 3</option>
             </Select>
+            <br></br><br></br>
+            Total budget
             <Select placeholder="Select option">
               <option value="option1">Option 1</option>
               <option value="option2">Option 2</option>
               <option value="option3">Option 3</option>
             </Select>
           </Text>
+          <br></br><br></br>
           <Button
           size="md"
             style={{
-              backgroundColor: "white",
+              backgroundColor: "#63B3ED",
               color: "black",
               borderColor: "black",
             }}
@@ -79,6 +87,8 @@ export const Project = () => {
           >
             Post Project
           </Button>
+          <br></br>
+          <br></br>
         </Box>
       </Center>
     </VStack>
