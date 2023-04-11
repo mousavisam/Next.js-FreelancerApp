a24dcdb391a3db18ad64d8335e01e4c35e45ea1c
Thamizhazhagan-Murugan <thamizhazhagan.ca@gmail.com>
2023-04-11 21:43:45 +0530

    added graph on dashboard

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -36,27 +36,109 @@ import {
   Flex,
   Avatar,
 } from "@chakra-ui/react";
+import Chart from "react-apexcharts";
+import { useState } from "react";
 export const Dashboard = () => {
-  return <>
-  <Stack spacing={8}>
-  <Box></Box>
-          <Box p={5} shadow="md" position="relative" left="400px" width="600px" borderWidth="1px" flex="1" borderRadius="sm">
+  const [product, setProduct] = useState([
+    {
+      name: "T-shirt",
+      data: [234, 45, 67, 987, 345, 456, 87, 321, 45, 569, 76, 890],
+    },
+    {
+      name: "Shirt",
+      data: [562, 145, 267, 97, 45, 156, 87, 321, 845, 969, 706, 20],
+    },
+    {
+      name: "Jeans",
+      data: [1012, 345, 117, 697, 845, 56, 287, 1321, 1845, 469, 306, 120],
+    },
+  ]);
+
+  const [option, setOption] = useState({
+    title: { text: "Product sell in 2021" },
+    xaxis: {
+      title: { text: "Product Sell in Months" },
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+    },
+    yaxis: {
+      title: { text: "Product in K" },
+    },
+  });
+  return (
+    <>
+      <Grid
+        templateAreas={`
+                  " main nav"
+                  "main nav"`}
+        gridTemplateRows={"50px 1fr 30px"}
+        gridTemplateColumns={"1050px 1fr"}
+        h="80vh"
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
+      >
+        <GridItem pl="2" area={"main"}>
+          <Box
+            shadow="md"
+            left="400px"
+            width="1000px"
+            borderWidth="1px"
+            borderRadius="sm"
+          >
             <Link fontSize="xl">User 1</Link>
-            
-            <Text mt={4}>Hello Freelancers! Hope you are doing well.
-We just wanted to let you know that this month we added these skills into the site:
-Adobe Workfront, Windchill PLM, Cloud Custodian, ChatGPT and Firewall
-If you work with these skills and want to add them you can do it from your profile, if you have any issue do not hesitate to contact support.</Text>
+
+            <Text mt={4}>
+              Hello Freelancers! Hope you are doing well. We just wanted to let
+              you know that this month we added these skills into the site:
+              Adobe Workfront, Windchill PLM, Cloud Custodian, ChatGPT and
+              Firewall If you work with these skills and want to add them you
+              can do it from your profile, if you have any issue do not hesitate
+              to contact support.
+            </Text>
             <Text mt={4}></Text>
           </Box>
           <Box></Box>
-          <Box p={5} shadow="md" position="relative" left="400px" width="600px" borderWidth="1px" flex="1" borderRadius="sm">
+          <Box
+            shadow="md"
+            left="400px"
+            width="1000px"
+            borderWidth="1px"
+            borderRadius="sm"
+          >
             <Link fontSize="xl">User 2</Link>
-            
-            <Text mt={4}>Freelancer blocks executable files in attachments for a reason. Be careful of clicking on Google Drive links or zip files as they may contain a virus. 
-            If you are suspicious of a file you can put the link into virustotal.com for scanning.</Text>
+
+            <Text mt={4}>
+              Freelancer blocks executable files in attachments for a reason. Be
+              careful of clicking on Google Drive links or zip files as they may
+              contain a virus. If you are suspicious of a file you can put the
+              link into virustotal.com for scanning.
+            </Text>
             <Text mt={4}></Text>
           </Box>
-  
-</Stack></>;
+        </GridItem>
+        <GridItem pl="2" bg="pink.300" area={"nav"}>
+          <Chart
+            type="line"
+            width={490}
+            height={350}
+            series={product}
+            options={option}
+          ></Chart>
+        </GridItem>
+      </Grid>
+    </>
+  );
 };
